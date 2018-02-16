83acb0d13089f96daaa21361b29e085366e44176
Gabriel <gabrielruss88@gmail.com>
2018-02-16 12:26:55 -0500

    about to add redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,12 +1,42 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    
+    this.state = {
+      course: { title: "" }
+    };
+
+    this.onClickSave = this.onClickSave.bind(this);
+    this.onTitleChange = this.onTitleChange.bind(this);
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course });
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          onClick={this.onClickSave} />
       </div>
-    ); 
+    );
   }
 }
 
